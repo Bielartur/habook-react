509f0e1474ad076e6176101c01e948dbcc2577a9
Gabriel Canto <gabrielcanto2016@gmail.com>
2025-12-07 15:57:38 -0300

    Adicionando a página de segurança

--- src/components/shared/buttons/ButtonSave.tsx
+++ src/components/shared/buttons/ButtonSave.tsx
@@ -2,10 +2,13 @@ import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
 import {faSave} from "@fortawesome/free-solid-svg-icons";
 import {ButtonGradient} from "./ButtonGradient.tsx";
 
+type Props = React.ButtonHTMLAttributes<HTMLButtonElement> & {
+    className?: React.ReactNode;
+};
 
-export const ButtonSave = () => {
+export const ButtonSave = ({ className = "", ...props }: Props) => {
     return (
-        <ButtonGradient className="mt-1 font-semibold">
+        <ButtonGradient className={`mt-1 font-semibold w-full ${className}`} {...props}>
             <FontAwesomeIcon icon={faSave} />
             Salvar alterações
         </ButtonGradient>
